10257a2a6295150f4d62d1ff9917ca63e3bdda58
islamGharib <islamahmed1992@yahoo.com>
2022-02-22 10:15:05 +0200

    adding orders states

---
+++ lib/shared/cubit/states.dart
@@ -0,0 +1,5 @@
+abstract class OrderStates{}
+class OrdersInitialState extends OrderStates{}
+class OrdersLoadingState extends OrderStates{}
+class OrdersSuccessState extends OrderStates{}
+class OrdersErrorState extends OrderStates{}
