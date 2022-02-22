b03ba4fd70eb81cf1a1fd1fae6f59c8c7e8eb503
islamGharib <islamahmed1992@yahoo.com>
2022-02-22 11:38:53 +0200

    handling json parsing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:ecommerce_shop/shared/bloc_provider.dart';
+import 'package:ecommerce_shop/shared/cubit/cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'modules/orders_metrics/orders.dart';
 
 void main() {
   Bloc.observer = MyBlocObserver();
@@ -11,9 +14,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Container(),
+    return BlocProvider(
+      create: (BuildContext context) => OrderCubit()..readOrdersJson(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: OrdersMetrics(),
+      ),
     );
   }
 }
